f7b9b4d223a2edcaa67004d304b4e7c3e65a9b30
ZLozZ <ironman242k2@gmail.com>
2024-03-08 00:32:32 +0700

    Add code test model

---
+++ test_camera.py
@@ -0,0 +1,24 @@
+
+import cv2
+from ultralytics import YOLO
+model = YOLO("D:\\Dev_Vision\\Project\\runs\detect\\train\\weights\\best.pt")
+
+# cap = cv2.VideoCapture("D:\\Dev_Vision\\Project\\WIN_20240114_14_02_53_Pro.mp4")
+cap = cv2.VideoCapture(1)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if success:
+        results = model(frame)
+        annotated_frame = results[0].plot()
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+        for result in results:
+            print("class", result.boxes.cls)
+            print("xyxy", result.boxes.xyxy)
+            print("conf", result.boxes.conf)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
