6a04b75cc9383d87a2ecb5c7fe7519d242dc56ea
ZLozZ <ironman242k2@gmail.com>
2024-03-16 23:29:42 +0700

    build a new model from YAML

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
+#model = YOLO('yolov8n.yaml')
 model = YOLO('yolov8n.pt')
 # model = YOLO("D:\\Dev_Vision\\Project\\runs\detect\\train\\weights\\last.pt")
 
 if __name__ == '__main__': # can khi chay bang GPU
-    model.train(data='dataset.yaml',epochs=20,imgsz=640,batch=24,optimizer='Adam')
+    model.train(data='dataset.yaml',epochs=50,imgsz=640,batch=2,optimizer='SGD')
     metrics = model.val()
