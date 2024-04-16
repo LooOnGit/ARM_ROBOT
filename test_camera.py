aa64b793121a09d96542ce6291e6e970a38875e6
ZLozZ <ironman242k2@gmail.com>
2024-04-16 23:39:28 +0700

    add file best

--- test_camera.py
+++ test_camera.py
@@ -1,10 +1,10 @@
 
 import cv2
 from ultralytics import YOLO
-model = YOLO("D:\\Dev_Vision\\ARM_ROBOT\\runs\\detect\\train3\\\weights\\best.pt")
 
-cap = cv2.VideoCapture("D:\\Dev_Vision\\Project\\all.mp4")
-#cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture("D:\\Dev_Vision\\video\\all.mp4")
+# cap = cv2.VideoCapture(1)
+model = YOLO("D:\\Dev_Vision\\ARM_ROBOT\\runs\\detect\\train\\weights\\best.pt")
 
 while cap.isOpened():
     success, frame = cap.read()
